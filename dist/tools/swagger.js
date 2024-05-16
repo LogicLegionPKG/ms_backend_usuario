1fe86c9440f57be290ac29d1f52efa60fe93968b
unknown <SENA@ITAAPRRCDMCD41.FormacionSena.RED>
2024-05-16 09:02:51 -0500

    Proyecto

---
+++ dist/tools/swagger.js
@@ -0,0 +1,23 @@
+"use strict";
+
+var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
+var _swaggerAutogen = _interopRequireDefault(require("swagger-autogen"));
+var _dotenv = require("dotenv");
+// const swaggerAutogen = require('swagger-autogen')();
+
+(0, _dotenv.config)();
+var port = process.env.PORT || 4000;
+var doc = {
+  info: {
+    title: 'Seguridad',
+    description: 'Manejo de usuarios'
+  },
+  host: 'localhost:' + port + "/api"
+};
+var outputFile = './swagger-output.json';
+var routes = ['../routes/routes.main.js', '../routes/routes.users.js'];
+
+/* NOTE: If you are using the express Router, you must pass in the 'routes' only the 
+root file where the route starts, such as index.js, app.js, routes.js, etc ... */
+
+(0, _swaggerAutogen["default"])(outputFile, routes, doc);
